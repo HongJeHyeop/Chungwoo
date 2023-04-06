da2c08d2049a82c0567c33a6295175cdaf2c6297
James Bon <007@mit.gov>
2023-04-06 21:47:26 +0900

    230406-회사소개인사말 생성

--- src/main/java/com/transport/cw/controller/MainController.java
+++ src/main/java/com/transport/cw/controller/MainController.java
@@ -3,17 +3,25 @@ package com.transport.cw.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.security.PermitAll;
 
 @Log4j2
 @Controller
+@RequestMapping("/")
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         log.info("====== 메인페이지 접속! =====");
         return "home/main";
+    }
+
+    @GetMapping("/introduce/main")
+    public void intro() {
+        log.info("====== 회사 소개 페이지 접속! =======");
+
     }
 
 }
