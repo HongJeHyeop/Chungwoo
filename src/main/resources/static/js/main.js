c7b701b09e19be157bd010c5e7a0d20d6f11da30
HongJeHyeop <jhh951217@naver.com>
2023-04-10 00:57:24 +0900

    230410-update

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,18 +1,30 @@
-const mainImg = document.getElementById('main-img-items');
-const imgLeftBtn = document.querySelector('#arrows').firstElementChild;
-const imgRightBtn = document.querySelector('#arrows').lastElementChild;
+const mainImg = document.getElementById('main-img-items').children;
+const imgChange = document.getElementById('img-num').children;
 
-console.log(imgRightBtn)
-console.log(imgLeftBtn)
 console.log(mainImg)
+console.log(imgChange)
 /*** 이미지 슬라이드 ***/
-imgLeftBtn.addEventListener('click', () => {
-    mainImg.style.transform += 'translateX(-100vw)';
-    console.log(mainImg)
-    console.log("이동");
-})
-imgRightBtn.addEventListener('click', () => {
-    mainImg.style.transform += 'translateX(100vw)';
-    console.log(mainImg.style.transform)
-    console.log("이동");
-})
+
+function img_changing(num, e) {
+    for (let img of mainImg) {
+        img.style.opacity = '0';
+    }
+    for (let changeBtn of imgChange) {
+        changeBtn.style.background = 'rgba(255, 255, 255, 0.4)';
+    }
+    switch (num) {
+        case 1:
+            mainImg.item(0).style.opacity = '100%';
+            e.style.background = 'rgba(255, 255, 255, 0.9)';
+            break;
+        case 2:
+            mainImg.item(1).style.opacity = '100%';
+            e.style.background = 'rgba(255, 255, 255, 0.9)';
+            break;
+        case 3:
+            mainImg.item(2).style.opacity = '100%';
+            e.style.background = 'rgba(255, 255, 255, 0.9)';
+            break;
+    }
+
+}
