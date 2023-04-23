9e588d267d3337c4aac3d1513fbe3349a3754671
HongJeHyeop <jhh951217@naver.com>
2023-04-23 19:19:24 +0900

    230423-update

--- src/main/resources/static/js/home/main.js
+++ src/main/resources/static/js/home/main.js
@@ -1,30 +1,53 @@
-const mainImg = document.getElementById('main-img-items').children;
-const imgChange = document.getElementById('img-num').children;
+const images = document.querySelectorAll('.main-img');
+const selectBtns = document.querySelectorAll('.select-btn');
 
-console.log(mainImg)
-console.log(imgChange)
+// 현재 이미지 번호
+let currentImageIndex = 0;
+
 /*** 이미지 슬라이드 ***/
+// 5초 간격으로 이미지 전환 함수 호출
+setInterval(change_image, 5000);
 
-function img_changing(num, e) {
-    for (let img of mainImg) {
-        img.style.opacity = '0';
-    }
-    for (let changeBtn of imgChange) {
-        changeBtn.style.background = 'rgba(255, 255, 255, 0.4)';
-    }
-    switch (num) {
-        case 1:
-            mainImg.item(0).style.opacity = '100%';
-            e.style.background = 'rgba(255, 255, 255, 0.9)';
-            break;
-        case 2:
-            mainImg.item(1).style.opacity = '100%';
-            e.style.background = 'rgba(255, 255, 255, 0.9)';
-            break;
-        case 3:
-            mainImg.item(2).style.opacity = '100%';
-            e.style.background = 'rgba(255, 255, 255, 0.9)';
-            break;
-    }
+// 버튼 클릭했을 시 해당 이미지로 이동
+selectBtns.forEach(btn => {
+    btn.addEventListener('click', select_image);
+});
 
+// 초기 이미지 버튼 색상 강조
+selectBtns[currentImageIndex].style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+
+// 이미지 전환 함수
+function change_image() {
+    // 현재 이미지 숨기기
+    images[currentImageIndex].classList.remove('active');
+    // 다음 이미지로 인덱스 증가
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    // 다음 이미지 보이기
+    images[currentImageIndex].classList.add('active');
+    // 선택한 버튼 색상 강조
+    selectBtns.forEach(btn => {
+        btn.style.backgroundColor = 'rgba(255, 255, 255, 0.3)';
+    });
+    selectBtns[currentImageIndex].style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
 }
+
+// 버튼으로 이미지 선택하는 함수
+function select_image(e) {
+    // 클릭한 버튼의 인덱스 가져오기
+    const i = parseInt(e.target.getAttribute('data-index'));
+    // 현재 이미지 숨기기
+    images[currentImageIndex].classList.remove('active');
+    // 선택한 이미지 보이기
+    images[i].classList.add('active');
+    // 선택한 버튼 활성
+    selectBtns[currentImageIndex].classList.remove('active');
+    selectBtns[i].classList.add('active');
+    // 선택한 버튼 색상 강조
+    selectBtns.forEach(btn => {
+        btn.style.backgroundColor = 'rgba(255, 255, 255, 0.3)';
+    });
+    selectBtns[i].style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
+    // 현재 이미지 인덱스 업데이트
+    currentImageIndex = i;
+}
+
