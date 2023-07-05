217c6c7238d7764d5eb6487949baf38078d0ff03
HongJeHyeop <jhh951217@naver.com>
2023-07-05 15:35:15 +0900

    230704 update

--- src/main/java/com/transport/cw/config/WebConfig.java
+++ src/main/java/com/transport/cw/config/WebConfig.java
@@ -16,5 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(resourcePath).addResourceLocations(uploacPath);
+        registry.addResourceHandler("/images/**").addResourceLocations("file:src/main/resources/static/images/");
     }
 }
