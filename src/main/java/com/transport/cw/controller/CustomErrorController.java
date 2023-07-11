59356b6268c35e8c4ff6356bf8b077270f733592
HongJeHyeop <jhh951217@naver.com>
2023-07-11 17:14:56 +0900

    Fix : modify error page (230711)

--- src/main/java/com/transport/cw/controller/CustomErrorController.java
+++ src/main/java/com/transport/cw/controller/CustomErrorController.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -14,7 +15,13 @@ import javax.servlet.http.HttpServletRequest;
 public class CustomErrorController implements ErrorController {
     @RequestMapping("/error")
     public String handle_error(HttpServletRequest request) {
-        log.info("에러페이지 이동");
-        return "/error/accessDenied";
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        log.info("Securing CustomError : " + status + " >> 에러페이지 이동");
+        return "/error/error";
+    }
+
+    @RequestMapping("/error/error")
+    public void error_page(){
+
     }
 }
