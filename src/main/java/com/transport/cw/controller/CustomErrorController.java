f101188bda9eb20a219d9b033baa6fb1e5c6eede
HongJeHyeop <jhh951217@naver.com>
2023-07-11 21:48:18 +0900

    Fix : modify error controller (230711)

--- src/main/java/com/transport/cw/controller/CustomErrorController.java
+++ src/main/java/com/transport/cw/controller/CustomErrorController.java
@@ -3,6 +3,7 @@ package com.transport.cw.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 @Log4j2
 @Controller
 public class CustomErrorController implements ErrorController {
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String handle_error(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         log.info("Securing CustomError : " + status + " >> 에러페이지 이동");
