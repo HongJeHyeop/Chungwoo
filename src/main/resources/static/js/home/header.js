aceda20c8645f005663b09bf29f1f0c744f63e45
HongJeHyeop <jhh951217@naver.com>
2023-07-24 15:09:23 +0900

    Add : responsive web design (230724)

--- src/main/resources/static/js/home/header.js
+++ src/main/resources/static/js/home/header.js
@@ -1,12 +1,13 @@
 const headerListTitle = document.getElementsByClassName('header-list-title');
-const headerDetailList = document.getElementsByClassName('header-detail-list');
-
+const headerDetailList =[...document.getElementsByClassName('header-detail-list')];
 function show_header_list(e) {
     e.querySelector('ul').style.display = "block";
 }
+
 function hide_header_list(e) {
     e.querySelector('ul').style.display = "none";
 }
+
 /*** 헤더 메뉴 보여주기 ***/
 for (let list of headerListTitle) {
     list.addEventListener('mouseover', () => {
@@ -24,3 +25,13 @@ for (let list of headerDetailList) {
         list.style.display = "none";
     })
 }
+const headerListToggle = document.getElementById('header-list');
+const headerToggleBtn = document.getElementById('header-menu-toggle').firstElementChild;
+let toggleCheck = false;
+headerToggleBtn.addEventListener('click', () => {
+    toggleCheck = !toggleCheck;
+    toggleCheck ? headerListToggle.classList.add('toggle-active') : headerListToggle.classList.remove('toggle-active');
+    for (let list of headerDetailList) {
+        list.remove();
+    }
+})
